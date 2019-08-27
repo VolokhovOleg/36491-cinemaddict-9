cea3472f5c765d9fc8d081b268e398e23e49b69e
VolokhovOleg <volokhov.com@gmail.com>
2019-08-27 04:16:52 +0300

    Правки-1

--- src/components/rank.js
+++ src/components/rank.js
@@ -1,10 +1,10 @@
 import {AbstractComponent} from './abstract-component.js';
 
 export class Rank extends AbstractComponent {
- getTemplate() {
+  getTemplate() {
     return `<section class="header__profile profile">
-  <p class="profile__rating">Movie Buff</p>
-  <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-</section>`;
+      <p class="profile__rating">Movie Buff</p>
+      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+    </section>`;
   }
 }
